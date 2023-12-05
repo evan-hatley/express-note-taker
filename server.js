e3507d79b235c79df1d5dae1b11747238223d62c
evan-hatley <143963975+evan-hatley@users.noreply.github.com>
2023-12-05 14:54:15 -0600

    Fixed pathing
    Added correct api-route pathways to save and load data. Also added comments to explain functionality.

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
+// Create all the needed dependencies and routes
 const express = require('express');
 const apiRoute = require('./routes/api-route');
 const htmlRoute = require('./routes/html-route');
 const PORT = process.env.PORT || 3001;
 const app = express();
-
+// Sets up the middleware to parse json data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
 
+app.use(express.static('public'));
+// Sets up the routes to the routes folder
 app.use(apiRoute);
 app.use(htmlRoute);
 
