927d8453bcf8095f2f350ea2cdcd859e4a984666
evan-hatley <143963975+evan-hatley@users.noreply.github.com>
2023-12-04 21:14:40 -0600

    Routing

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-// Create routes and dependencies
 const express = require('express');
 const apiRoute = require('./routes/api-route');
 const htmlRoute = require('./routes/html-route');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Create the middleware with Express
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
